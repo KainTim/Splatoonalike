6740f8f173fe1ee28858d0b02978559f885df84b
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2025-05-30 15:32:03 +0200

    added Better Splotches

--- Assets/Scripts/WeaponScripts/CombineInk.cs
+++ Assets/Scripts/WeaponScripts/CombineInk.cs
@@ -1,36 +1,25 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace WeaponScripts
 {
   public class CombineInk : MonoBehaviour
   {
-    private float _ammoCanCollideTime;
-    public float AmmoCanCollideDelay = 0.3f;
+    public float AmmoCanCollideDelay = 1f;
+    public float CombineScale = 0.4f;
+    public float MaxSize = 5;
+    private Collider _collider;
     private void Awake()
     {
-      _ammoCanCollideTime = Time.time + AmmoCanCollideDelay;
+      _collider = GetComponent<Collider>();
+      _collider.enabled = false;
+      StartCoroutine(EnableColliderAfterDelay(AmmoCanCollideDelay));
     }
     private void OnTriggerEnter(Collider other)
     {
       if (other is null) return;
-      if (Time.time > _ammoCanCollideTime)
-      {
-        HandleInkCollision(other);
-      }
-      else if(other.gameObject.transform.parent.localScale.x > 5f ||
-              transform.parent.localScale.x > 5f)
-      {
-        Debug.Log($"Collided from Scale {transform.parent.localScale}");
-        Debug.Log($"Collided with Scale{other.gameObject.transform.parent.localScale}");
-      }
-    }
-    private void OnTriggerStay(Collider other)
-    {
-      if (Time.time > _ammoCanCollideTime)
-      {
-        HandleInkCollision(other);
-      }
+      HandleInkCollision(other);
     }
     private bool HandleInkCollision(Collider other)
     {
@@ -38,19 +27,33 @@ namespace WeaponScripts
       if (ink is null) return false;
       if (transform.parent.transform.localScale.x < other.transform.parent.transform.localScale.x) return false;
       if (GetInstanceID() < ink.GetInstanceID()) return false;
+
+      var ammo = transform.parent.gameObject.GetComponent<Ammo>();
       var scale = transform.parent.localScale;
-      scale.x += 0.2f;
-      scale.z += 0.2f;
+      var otherScale = other.transform.parent.localScale;
+      if (scale.x >= MaxSize || scale.z >= MaxSize) return false;
+      if (otherScale.x >= MaxSize || otherScale.z >= MaxSize) return false;
+
+      if (ammo.Damage < 1)
+      {
+        scale.x += CombineScale;
+        scale.z += CombineScale;
+      }
+      else
+      {
+        scale.x += CombineScale * (ammo.Damage/20f);
+        scale.z += CombineScale * (ammo.Damage/20f);
+      }
+      transform.parent.position = (other.transform.parent.position+transform.parent.position)/2;
       transform.parent.localScale = scale;
       Destroy(other.gameObject.transform.parent.gameObject);
-      var ammo = transform.parent.gameObject.GetComponent<Ammo>();
-      if (other.gameObject.transform.parent.localScale.x > 5f)
-      {
-        Debug.Log($"Destroyed with {ammo.Damage} Damage");
-        Debug.Log($"Destroyed from Scale {transform.parent.localScale}");
-        Debug.Log($"Destroyed GO with Scale{other.gameObject.transform.parent.localScale}");
-      }
       return true;
+    }
+
+    private IEnumerator EnableColliderAfterDelay(float delay)
+    {
+      yield return new WaitForSeconds(0.4f);
+      _collider.enabled = true;
     }
   }
 }
